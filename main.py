6443fcdcf4ea2d009d9f1c1a5a1dfefb92fe0874
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-06-17 00:17:43 -0500

    Format

--- main.py
+++ main.py
@@ -18,25 +18,35 @@ def epub_to_html(epub_path, html_path):
 
     for item in book.get_items():
         if item.get_type() == ebooklib.ITEM_IMAGE:
-            img_data = base64.b64encode(item.get_content()).decode('utf-8')
-            img_tags[
-                item.get_name()] = f'<img src="data:image/{item.media_type.split("/")[-1]};base64,{img_data}" style="max-width: 100%; height: auto;">'
+            img_data = base64.b64encode(item.get_content()).decode("utf-8")
+            img_tags[item.get_name()] = (
+                f'<img src="data:image/{item.media_type.split("/")[-1]};base64,{img_data}" style="max-width: 100%; '
+                f'height: auto;">'
+            )
 
     for item in book.get_items():
         if item.get_type() == ebooklib.ITEM_DOCUMENT:
-            content = item.get_content().decode('utf-8')
+            content = item.get_content().decode("utf-8")
             for img_name, img_tag in img_tags.items():
-                content = re.sub(r'src=["\'].*?' + re.escape(img_name) + '.*?["\'].*?>', img_tag, content)
+                content = re.sub(
+                    r'src=["\'].*?' + re.escape(img_name) + ".*?[\"'].*?>",
+                    img_tag,
+                    content,
+                )
             html_content += content
 
-    with open(html_path, 'w', encoding='utf-8') as html_file:
+    with open(html_path, "w", encoding="utf-8") as html_file:
         html_file.write(html_content)
 
 
-parser = argparse.ArgumentParser(description='Convert an epub file to a single html file.')
-parser.add_argument('epub_path', type=str, help='The path to the epub file.')
-parser.add_argument('--html_path', type=str, default=os.getcwd() + '/output.html',
-                    help='The path to the output html file. Default is current directory.')
+parser = argparse.ArgumentParser(description="Convert an epub file to a single html file.")
+parser.add_argument("epub_path", type=str, help="The path to the epub file.")
+parser.add_argument(
+    "--html_path",
+    type=str,
+    default=os.getcwd() + "/output.html",
+    help="The path to the output html file. Default is current directory.",
+)
 
 args = parser.parse_args()
 
